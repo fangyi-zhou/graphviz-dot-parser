cd207b62c792cbd7db41cb605f2a6f7d5ce2314e
Fangyi Zhou <me@fangyi.io>
2020-11-20 20:43:09 +0000

    Handle some whitespace

--- src/parser.rs
+++ src/parser.rs
@@ -2,15 +2,22 @@ extern crate nom;
 
 use nom::branch::alt;
 use nom::bytes::complete::tag_no_case;
+use nom::character::complete::space0;
 use nom::combinator::{map, opt};
 use nom::IResult;
 
 fn parse_strict(s: &str) -> IResult<&str, bool> {
-    map(opt(tag_no_case("strict")), |tag| tag.is_some())(s)
+    let (s, _) = space0(s)?;
+    let (s, out) = map(opt(tag_no_case("strict")), |tag| tag.is_some())(s)?;
+    let (s, _) = space0(s)?;
+    Ok((s, out))
 }
 
 fn parse_directed(s: &str) -> IResult<&str, bool> {
+    let (s, _) = space0(s)?;
     let indirected = map(tag_no_case("graph"), |_| false);
     let directed = map(tag_no_case("digraph"), |_| true);
-    alt((directed, indirected))(s)
+    let (s, out) = alt((directed, indirected))(s)?;
+    let (s, _) = space0(s)?;
+    Ok((s, out))
 }
