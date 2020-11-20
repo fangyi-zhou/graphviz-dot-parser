200f4977a4c17437c59c14ff382050576eefbf2b
Fangyi Zhou <me@fangyi.io>
2020-11-20 19:53:38 +0000

    Add some types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+use std::collections::HashMap;
+
+pub struct GraphAST {
+    pub is_strict: bool,
+    pub is_directed: bool,
+    pub id: String,
+    pub stmt: Vec<Stmt>,
+}
+
+pub enum AttributeType {
+    Graph,
+    Node,
+    Edge,
+}
+
+pub type Attributes = HashMap<String, String>;
+
+pub enum Stmt {
+    // port is unsupported
+    Node(String, Attributes),
+    Edge(String, String),
+    Attr(AttributeType, Attributes),
+    Assign(String, String),
+    SubGraph(Option<String>, Vec<Stmt>),
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
