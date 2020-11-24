495c940385c131973bc630fba236d1f41a4e0d7f
Fangyi Zhou <me@fangyi.io>
2020-11-24 18:10:48 +0000

    Convert to petgraph

--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,57 @@
+use petgraph::Graph;
 use std::collections::HashMap;
+
 pub struct GraphAST {
     pub is_strict: bool,
     pub is_directed: bool,
     pub id: Option<String>,
     pub stmt: Vec<Stmt>,
+}
+
+impl GraphAST {
+    pub fn to_directed_graph(&self) -> Option<Graph<String, ()>> {
+        if self.is_directed {
+            let mut g = Graph::new();
+            let mut nodes = HashMap::new();
+            for stmt in &self.stmt {
+                match stmt {
+                    Stmt::Node(n, _) => {
+                        let idx = g.add_node(n.clone());
+                        nodes.insert(n, idx);
+                    }
+                    Stmt::Edge(n1, n2) => {
+                        g.add_edge(*nodes.get(&n1).unwrap(), *nodes.get(&n2).unwrap(), ());
+                    }
+                    _ => {}
+                }
+            }
+            Some(g)
+        } else {
+            None
+        }
+    }
+
+    pub fn to_undirected_graph(&self) -> Option<Graph<String, (), petgraph::Undirected>> {
+        if !self.is_directed {
+            let mut g = Graph::new_undirected();
+            let mut nodes = HashMap::new();
+            for stmt in &self.stmt {
+                match stmt {
+                    Stmt::Node(n, _) => {
+                        let idx = g.add_node(n.clone());
+                        nodes.insert(n, idx);
+                    }
+                    Stmt::Edge(n1, n2) => {
+                        g.add_edge(*nodes.get(&n1).unwrap(), *nodes.get(&n2).unwrap(), ());
+                    }
+                    _ => {}
+                }
+            }
+            Some(g)
+        } else {
+            None
+        }
+    }
 }
 
 #[derive(Eq, PartialEq, Debug)]
